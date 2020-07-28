90f64ec8140ffa4452eedae918ce96820396ce8a
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-07-28 07:07:06 -0600

    first commit

---
+++ Box/src/Box.java
@@ -0,0 +1,22 @@
+
+class Box {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int size = 0;
+	}
+
+	
+	public static void printBox(int size, char star) {
+	{
+	star = '*';
+	for (int i = 0 ; i < size ; i++)
+		System.out.print(star);	
+	
+
+	}
+
+	}
+
+}
