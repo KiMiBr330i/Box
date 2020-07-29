3272de2dfb4f2a52787689617d7f8a6c0bcd3ab0
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-07-28 22:07:56 -0600

    fourth commit

--- Box/src/Box.java
+++ Box/src/Box.java
@@ -4,20 +4,32 @@ class Box {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-	}
-	public static int size = 0;
-		
+	
+	int size = 0;
+	Box box5 = new Box();	
+	box5.setSize();
+	box5.printBox();
+	
 		public static void printBox() {
 		
 			char star = '*';
 			int max = size ;
 			for (int i = 0 ; i < max ; i++) {
 				for (int j=i ; j < max ; j++)
+		
 					System.out.print(star); }
+
+	
+	
+	
+
+		}	
+	
+		public static setSize(size1) {
+			int size1 = 5;
+	}
+		public static getSize() {
+			return size1;
 	}
 	
-	public static void getSize() {
-		
-	
-}
-}
+	}}
