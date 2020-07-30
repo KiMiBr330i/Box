d68fc5679cc083dc0c97e0fe01f00315d416b500
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-07-29 19:32:43 -0600

    seventh commit

--- Box/src/Box.java
+++ Box/src/Box.java
@@ -1,36 +1,33 @@
-
+//---------------------------------------------------------------------------------
+// Lab3 Exercise 4 Box project
+//  Kip Brower CPSC 50100-002
+// A program that prints a box of stars of a given size and row value.
+//---------------------------------------------------------------------------------
 class Box {
 
 	int size = 0;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			
-	
-	int size = 0;
-	Box box5 = new Box();	
-	box5.setSize(5);
-	box5.printBox();
-	}
-	
 
-		public void printBox() {
-		
-		char star = '*';
-		int max = size;
-		for (int i = 0 ; i < max ; i++) {
-			//for (int j=i ; j < max ; j++)
-	
-				System.out.print(star);
-				}
-		}
-		
-		public void setSize(int size) {
-			this.size = 5;
-		}
-		public int getSize() {
-			return size;
-		}
-		
+	public static void main(String[] args) {
+
+		Box box5 = new Box(); // New Box object created
+		box5.setSize(5); //invoked setter method to change the size variable
+		box5.printBox(); 
 	}
-	
+	//method to print asterisk character is box form
+	public void printBox() {
+
+		char star = '*';
+
+		for (int row = 1; row <= size; row++) { // row print
+			for (int column = 1; column <= size; column++) // column print
+
+				System.out.print(star);
+			System.out.println();
+		}
+	}
+	//Setter to change the size value for rows and columns
+	public void setSize(int size) {
+		size = 5;
+	}
+
+}
